0ad585afb3bf250573eda5f4780efe64fa16a6d9
Ganu16-N <ganunagargoje16@gmail.com>
2023-08-22 17:58:35 +0530

    initial commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,68 @@
+import React, { useContext, useEffect } from "react";
+
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
+import VideoCard from "./VideoCard";
+import { AiFillHome, AiOutlineFlag } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+import { MdLocalFireDepartment, MdOutlineSubscriptions, MdOutlineVideoLibrary } from "react-icons/md";
+
+const Feed = () => {
+    const { loading, searchResults } = useContext(Context);
+
+    useEffect(() => {
+        document.getElementById("root").classList.remove("custom-h");
+    }, []);
+
+    return (
+        <div className="flex flex-row h-[calc(100%-56px)]">
+
+            <div className="text-[0.7rem]  w-[80px] bg-black text-white flex flex-col items-center pt-6 gap-6">
+
+                <NavLink to='/'>
+                    <div className=" flex flex-col items-center justify-center">
+                        <AiFillHome className="text-[1.2rem] mb-1" />
+                        <div>Home</div>
+                    </div>
+                </NavLink>
+
+                <NavLink to='#'>
+                    <div className=" flex flex-col items-center justify-center">
+                        <MdLocalFireDepartment className="text-[1.2rem] mb-1"/>
+                        <div>Shorts</div>
+                    </div>
+                </NavLink>
+                <NavLink to='#'>
+                    <div className=" flex flex-col items-center justify-center">
+                        <MdOutlineSubscriptions className="text-[1.2rem] mb-1"/>
+                        <div>Subscriptions</div>
+                    </div>
+                </NavLink>
+                <NavLink to='#'>
+                    <div className=" flex flex-col items-center justify-center">
+                        <MdOutlineVideoLibrary className="text-[1.2rem] mb-1"/>
+                        <div>Library</div>
+                    </div>
+                </NavLink>
+            </div>
+
+            <LeftNav />
+            <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+                    {!loading &&
+                        searchResults.map((item) => {
+                            if (item.type !== "video") return false;
+                            return (
+                                <VideoCard
+                                    key={item?.video?.videoId}
+                                    video={item?.video}
+                                />
+                            );
+                        })}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Feed;
---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":
+        "a77ce8ab38msh1cd7d733ee2fa27p195b7ejsnfbdda9bdb295" || process.env.REACT_APP_YOUTUBE_API_KEY ,
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
